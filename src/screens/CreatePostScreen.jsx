4441ec7b0bc8ff50eaeb5fee294608cfdb6e42ac
Kushagra102 <kushagra.garg77@gmail.com>
2024-02-27 23:16:21 +0530

    Create Post Screen Completed

---
+++ src/screens/CreatePostScreen.jsx
@@ -0,0 +1,102 @@
+import { View, Text, StyleSheet, Image, TextInput, Button } from "react-native";
+import { useState } from "react";
+import { Entypo } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+
+const user = {
+  id: "u1",
+  image:
+    "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg",
+  name: "Plarck Cacil",
+};
+
+const CreatePostScreen = () => {
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState(null);
+
+  const onSubmit = () => {
+    console.warn("Post Submitted", description);
+    setDescription("");
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={{ uri: user.image }} style={styles.profileImage} />
+        <Text style={styles.name}>{user.name}</Text>
+        <Entypo
+          onPress={pickImage}
+          name="images"
+          size={24}
+          color="limegreen"
+          style={styles.icon}
+        />
+      </View>
+
+      <TextInput
+        value={description}
+        onChangeText={setDescription}
+        placeholder="What is on your mind ?"
+        multiline
+      />
+      <Image source={{ uri: image }} style={styles.image} />
+      <View style={styles.buttonContainer}>
+        <Button title="Post" onPress={onSubmit} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    padding: 10,
+    paddingTop: 35,
+    backgroundColor: "white",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 10,
+  },
+  profileImage: {
+    height: 45,
+    width: 45,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+  name: {
+    fontWeight: "500",
+  },
+  buttonContainer: {
+    marginTop: "auto",
+    marginVertical: 10,
+  },
+  icon: {
+    marginLeft: "auto",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    alignSelf: "center",
+  },
+});
+
+export default CreatePostScreen;
